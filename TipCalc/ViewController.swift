3d9d725ce7d65f4e13f2368acc4d0f286f03c0c5
RenuBandaru <renusree.bandaru@gmail.com>
2020-10-07 00:19:00 -0400

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Renu Bandaru on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
